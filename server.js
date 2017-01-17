2091371c0ad881d5aecbd237d436246396805844
ShiDongjie <dongjie.shi@baidao.com>
2017-01-17 18:19:33 +0800

    remove promise

--- server.js
+++ server.js
@@ -3,7 +3,5 @@ import {createApp} from './index';
 export default function (options) {
     const app = createApp(options);
 
-    return new Promise(resolve => {
-        resolve(app);
-    });
+    return app;
 }
